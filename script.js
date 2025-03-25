c6fc04010d7d0ad359529ad6ac4e30e3dd4081f4
Siddanta Sodari <siddantasodari123@gmail.com>
2025-03-25 23:02:42 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+JavaScript (script.js):
+
+const mobileMenu = document.getElementById("mobile-menu");
+const navList = document.querySelector(".nav-list");
+
+mobileMenu.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
